f4b84bd67bce1f1d0520714bfb5f55bf1eaec2f2
xfnw <xfnw@riseup.net>
2023-01-14 14:15:21 -0500

    add list utility

---
+++ list.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env zsh
+
+DOMAINS=${1:-domains}
+
+getcerts(){
+
+	for domain port in $(<$DOMAINS)
+	do
+		{
+			echo "$(catgirl -h $domain -p $port -o | expdays || echo DOWN)\t$domain"
+			echo "got $domain:$port" 1>&2
+		} &
+		sleep 0.1
+	done
+
+}
+
+
+sortedcerts(){
+
+	getcerts | sort -rh
+
+}
+
+sortedcerts
+
